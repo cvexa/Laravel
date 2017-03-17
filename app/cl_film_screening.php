1f8372c6603dd467037ce5c1a8d1aeb53c3d7e29
Svetoslav <cvexxxxa@abv.bg>
2017-03-18 01:48:13 +0200

    fillable
    fillable screening model

--- app/cl_film_screening.php
+++ app/cl_film_screening.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class cl_film_screening extends Model
 {
+	 protected $fillable = [
+	    'date', 'hour', 'cm_movie_id','price','free_seats'
+    ];
+
     public function cmMovie()
     {
     	return $this->belongsTo(cm_movie::class, 'cm_movie_id');
